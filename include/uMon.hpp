3e1ec1eff8dbb8b1c3a25d18ee1ba7506d8a228a
Trevor Makes <the.trevor.makes@gmail.com>
2022-02-23 21:35:05 -0800

    Add hex dump command

--- include/uMon.hpp
+++ include/uMon.hpp
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include "uCLI.hpp"
+
 #include <stdint.h>
 
 namespace uMon {
@@ -34,6 +36,45 @@ template <typename F>
 void fmt_hex32(F&& print, uint32_t n) {
   fmt_hex16(print, n >> 16);
   fmt_hex16(print, n & 0xFFFF);
+}
+
+template <typename API, uint8_t COL_SIZE = 16>
+void hex_dump(uint16_t start, uint16_t end) {
+  uint8_t row_data[COL_SIZE];
+  for (uint16_t row = start; row < end; row += COL_SIZE) {
+    API::read_bytes(row, row_data);
+
+    // Print address
+    fmt_hex16(API::print_char, row);
+    API::print_char(':');
+
+    // Print hex data
+    for (uint8_t col = 0; col < COL_SIZE; ++col) {
+      API::print_char(' ');
+      if (col % 4 == 0) {
+        API::print_char(' ');
+      }
+      fmt_hex8(API::print_char, row_data[col]);
+    }
+
+    // Print string data
+    API::print_string("  \"");
+    for (uint8_t col = 0; col < COL_SIZE; ++col) {
+      char m = row_data[col];
+      if (m < ' ' || m >= 0x7F) {
+        m = '.'; // Display control and non-ASCII as dot
+      }
+      API::print_char(m);
+    }
+    API::print_string("\"\n");
+  }
+}
+
+template <typename API, uint8_t COL_SIZE = 16>
+void cmd_hex(uCLI::Tokens args) {
+  uint16_t start = args.has_next() ? parse_u32(args.next()) : 0;
+  uint16_t end = args.has_next() ? parse_u32(args.next()) : start + COL_SIZE;
+  hex_dump<API, COL_SIZE>(start, end);
 }
 
 } // namespace uMon
