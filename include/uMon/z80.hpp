5106410f9aff1464f8d150a9075f9cd4804fff65
Trevor Makes <the.trevor.makes@gmail.com>
2022-03-09 23:24:58 -0800

    Add initial asm tokenizer

--- include/uMon/z80.hpp
+++ include/uMon/z80.hpp
@@ -3,11 +3,97 @@
 
 #pragma once
 
+#include "z80/asm.hpp"
 #include "z80/dasm.hpp"
 #include "uCLI.hpp"
 
 namespace uMon {
 namespace z80 {
+
+template <typename API>
+void cmd_asm(uCLI::Args args) {
+  uMON_EXPECT_UINT(uint16_t, start, args);
+
+  const char* mne_str = args.next();
+  uint8_t mne = parse_mnemonic(mne_str);
+  uMON_FMT_ERROR(mne == MNE_INVALID, "mnemonic", mne_str);
+
+  // TODO just print for now
+  API::print_string(MNE_STR[mne]);
+  API::print_char(' ');
+
+  // Parse up to 3 operands
+  // NOTE only need 2 for documented ops; 3 for 
+  uint8_t n_ops = 0;
+  Operand operands[3];
+  for (; n_ops < 3 && args.has_next(); ++n_ops) {
+    uCLI::Tokens opr_tok = args.split_at(',');
+
+    operands[n_ops].token = 0;
+    operands[n_ops].value = 0;
+
+    // Handle indirect operand surronded by parentheses
+    if (opr_tok.peek_char() == '(') {
+      operands[n_ops].token = TOK_INDIRECT;
+      opr_tok.split_at('(');
+      opr_tok = opr_tok.split_at(')');
+
+      // Split optional displacement following +/-
+      uCLI::Tokens disp_tok = opr_tok;
+      bool is_minus = false;
+      disp_tok.split_at('+');
+      if (!disp_tok.has_next()) {
+        disp_tok = opr_tok;
+        disp_tok.split_at('-');
+        is_minus = true;
+      }
+
+      // Parse displacement and apply sign
+      uMON_OPTION_UINT(uint16_t, disp, disp_tok, 0);
+      if (disp != 0) {
+        operands[n_ops].value = is_minus ? -disp : disp;
+      }
+    }
+
+    // Parse operand as number or token
+    auto opr_str = opr_tok.next();
+    uint16_t tmp; // TODO
+    if (uMon::parse_unsigned(opr_str, tmp)) {
+      operands[n_ops].token |= TOK_INTEGER;
+      operands[n_ops].value = tmp;
+    } else {
+      uint8_t token = parse_token(opr_str);
+      uMON_FMT_ERROR(token == TOK_INVALID, "token", opr_str);
+      operands[n_ops].token |= token;
+    }
+
+    // TODO just print for now
+    uint8_t token = operands[n_ops].token;
+    uint16_t value = operands[n_ops].value;
+    if ((token & TOK_INDIRECT) != 0) {
+      API::print_char('*');
+      token &= ~TOK_INDIRECT;
+    }
+    if (token < TOK_INVALID) {
+      API::print_string(TOK_STR[token]);
+      if (value != 0) {
+        API::print_char('+');
+        fmt_hex16(API::print_char, value);
+      }
+    } else if (token == TOK_INTEGER) {
+      fmt_hex16(API::print_char, value);
+    } else {
+      API::print_char('?');
+    }
+    API::print_char(',');
+  }
+
+  // TODO just print for now
+  API::print_char('\n');
+
+  uint16_t next = /* TODO */ start;
+  set_prompt<API>(args.command(), next);
+}
 
 template <typename API>
 void cmd_dasm(uCLI::Args args) {
---
+++ include/uMon/z80/asm.hpp
@@ -0,0 +1,39 @@
+// https://github.com/trevor-makes/uMon.git
+// Copyright (c) 2022 Trevor Makes
+
+#pragma once
+
+#include "uMon/z80/common.hpp"
+#include "uMon/format.hpp"
+#include "uCLI.hpp"
+
+#include <stdint.h>
+
+namespace uMon {
+namespace z80 {
+
+struct Operand {
+  uint8_t token;
+  uint16_t value;
+};
+
+uint8_t parse_mnemonic(const char* str) {
+  for (uint8_t i = 0; i < MNE_INVALID; ++i) {
+    if (strcasecmp(str, MNE_STR[i]) == 0) {
+      return i;
+    }
+  }
+  return MNE_INVALID;
+}
+
+uint8_t parse_token(const char* str) {
+  for (uint8_t i = 0; i < TOK_INVALID; ++i) {
+    if (strcasecmp(str, TOK_STR[i]) == 0) {
+      return i;
+    }
+  }
+  return TOK_INVALID;
+}
+
+} // namespace z80
+} // namespace uMon
