82072e4e2521aad4a376b2e5ec2bc546131d439e
Trevor Makes <the.trevor.makes@gmail.com>
2022-03-19 01:20:23 -0700

    Add mem move command

--- include/uMon.hpp
+++ include/uMon.hpp
@@ -63,6 +63,30 @@ uint16_t impl_strcpy(uint16_t start, const char* str) {
   }
 }
 
+// Copy [start, end] to [dest, dest+end-start] (end inclusive)
+template <typename API>
+void impl_memmove(uint16_t start, uint16_t end, uint16_t dest) {
+  uint16_t delta = end - start;
+  uint16_t dest_end = dest + delta;
+  // Buses narrower than 16-bits introduce cases with ghosting (wrap-around).
+  // This logic should work as long as start and dest are both within [0, 2^N),
+  // where N is the actual bus width.
+  bool a = dest <= end;
+  bool b = dest_end < start;
+  bool c = dest > start;
+  if ((a && b) || (a && c) || (b && c)) {
+    // Reverse copy from end to start
+    for (uint16_t i = 0; i <= delta; ++i) {
+      API::write_byte(dest_end - i, API::read_byte(end - i));
+    }
+  } else {
+    // Forward copy from start to end
+    for (uint16_t i = 0; i <= delta; ++i) {
+      API::write_byte(dest + i, API::read_byte(start + i));
+    }
+  }
+}
+
 template <typename API, uint8_t COL_SIZE = 16>
 void cmd_hex(uCLI::Args args) {
   // Default size to one row if not provided
@@ -91,6 +115,14 @@ void cmd_fill(uCLI::Args args) {
   uMON_EXPECT_UINT(uint16_t, size, args);
   uMON_EXPECT_UINT(uint8_t, pattern, args);
   impl_memset<API>(start, start + size - 1, pattern);
+}
+
+template <typename API>
+void cmd_move(uCLI::Args args) {
+  uMON_EXPECT_UINT(uint16_t, start, args);
+  uMON_EXPECT_UINT(uint16_t, size, args);
+  uMON_EXPECT_UINT(uint16_t, dest, args);
+  impl_memmove<API>(start, start + size - 1, dest);
 }
 
 } // namespace uMon
